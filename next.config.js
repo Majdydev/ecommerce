86c2347d256b39b5503384ae0f25ac218f0b917b
majd <majdabbassi27@gmial.com>
2025-05-12 11:56:45 +0100

    eslint ignore

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-    domains: ['images.pexels.com', 'th.bing.com'],
+    domains: ["images.pexels.com", "th.bing.com", "www.android.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "th.bing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.android.com",
+      },
+    ],
   },
 };
 
