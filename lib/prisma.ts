4d2ddffa2be305edc01f75009af3d498b0252a27
majd <majdabbassi27@gmial.com>
2025-05-12 11:36:47 +0100

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,7 +6,34 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+// Create a new PrismaClient with better error logging
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
+
+// Add error logging
+prisma.$on("error" as never, (e) => {
+  console.error("Prisma Client error:", e);
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
